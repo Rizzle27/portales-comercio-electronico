8450f8154cfb769d996993b5e848c26c59d9e52a
Lucas Emanuel Garcia <lucas_17_garcia@outlook.com>
2023-08-27 08:23:56 +0900

    Primer commit

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Las rutas son combinaciones de un método HTTP con una URL tomada a partir de la carpeta public del framework y se asocian a una acción.
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
-// Route::get('/quienes-somos', [\App\Http\Controllers\HomeController::class, 'about']);
-
 // Route::get('/peliculas/listado', [\App\Http\Controllers\HomeController::class, 'movies']);
 
 Route::get('/paises/lista', [\App\Http\Controllers\HomeController::class, 'paises']);
